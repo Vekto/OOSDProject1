90515b81a3eff5b9f3b9acb16db8ce806e34c182
Vekto <Davison.chad@gmail.com>
2015-11-10 12:24:37 -0700

    more database
    new full DB

--- TravelExperts.sql
+++ TravelExperts.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2015 at 09:19 PM
+-- Generation Time: Nov 10, 2015 at 12:14 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -666,18 +666,19 @@ CREATE TABLE `packages` (
   `PkgEndDate` datetime DEFAULT NULL,
   `PkgDesc` varchar(50) DEFAULT NULL,
   `PkgBasePrice` decimal(19,4) NOT NULL,
-  `PkgAgencyCommission` decimal(19,4) DEFAULT NULL
+  `PkgAgencyCommission` decimal(19,4) DEFAULT NULL,
+  `PkgImageUrl` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `packages`
 --
 
-INSERT INTO `packages` (`PackageId`, `PkgName`, `PkgStartDate`, `PkgEndDate`, `PkgDesc`, `PkgBasePrice`, `PkgAgencyCommission`) VALUES
-(1, 'Caribbean New Year', '2005-12-25 00:00:00', '2006-01-04 00:00:00', 'Cruise the Caribbean & Celebrate the New Year.', '4800.0000', '400.0000'),
-(2, 'Polynesian Paradise', '2005-12-12 00:00:00', '2005-12-20 00:00:00', '8 Day All Inclusive Hawaiian Vacation', '3000.0000', '310.0000'),
-(3, 'Asian Expedition', '2006-05-14 00:00:00', '2006-05-28 00:00:00', 'Airfaire, Hotel and Eco Tour.', '2800.0000', '300.0000'),
-(4, 'European Vacation', '2005-11-01 00:00:00', '2005-11-14 00:00:00', 'Euro Tour with Rail Pass and Travel Insurance', '3000.0000', '280.0000');
+INSERT INTO `packages` (`PackageId`, `PkgName`, `PkgStartDate`, `PkgEndDate`, `PkgDesc`, `PkgBasePrice`, `PkgAgencyCommission`, `PkgImageUrl`) VALUES
+(1, 'Caribbean New Year', '2005-12-25 00:00:00', '2006-01-04 00:00:00', 'Cruise the Caribbean & Celebrate the New Year.', '4800.0000', '400.0000', ''),
+(2, 'Polynesian Paradise', '2005-12-12 00:00:00', '2005-12-20 00:00:00', '8 Day All Inclusive Hawaiian Vacation', '3000.0000', '310.0000', ''),
+(3, 'Asian Expedition', '2006-05-14 00:00:00', '2006-05-28 00:00:00', 'Airfaire, Hotel and Eco Tour.', '2800.0000', '300.0000', ''),
+(4, 'European Vacation', '2005-11-01 00:00:00', '2005-11-14 00:00:00', 'Euro Tour with Rail Pass and Travel Insurance', '3000.0000', '280.0000', '');
 
 -- --------------------------------------------------------
 
