d6620c0dacc08f8c80d1c71b9a817818291c0a81
Emile-Leger <eeilm@hotmail.com>
2015-11-11 11:20:53 -0700

    variables
    an external file i used for some error messages, feel free to
    change/delete/not use. you'll have to do the include yourself.

---
+++ testingVars.php
@@ -0,0 +1,14 @@
+<?php
+/*
+// This file will contain all the variables used with my SQL
+// Generator functions.
+//===============================================================*/
+
+$deleteFail = "Delete failed! Please try again.";
+$deleteSuccess = "Delete was successful!";
+$updateFail = "Update Failed! Please try again.";
+$updateSuccess = "Update was successful!";
+$addFail = "Failed to add agent! Please try again.";
+$addSuccess = "Succesfully added agent!";
+$result;
+?>
